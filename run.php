a91c6d2da4fdcc294dd86d08d0fad0d31aa19a53
Jonas Andriunaitis <andriunaitis@gmail.com>
2023-06-30 12:06:59 +0300

    add csv only loading and printing case

--- run.php
+++ run.php
@@ -1,3 +1,30 @@
 <?php
+define("DATA_DIR", "./data");
 
-echo('hello world');
+function loadCSV(string $fname)
+{
+    if (($handle = fopen(DATA_DIR . "/" . $fname, "r")) === false) {
+        return [];
+    }
+
+    $res = [];
+    
+    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+        $res[] = $data;
+    }
+
+    fclose($handle);
+
+    return $res;
+}
+
+function run(string $fname)
+{
+    $data = loadCSV($fname);
+
+    print_r($data);
+
+    return 0;
+}
+
+run($argv[1]);
