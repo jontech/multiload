c03bd364e3a4eb7e3603ccc9d6dd8530a00fb67f
Jonas Andriunaitis <andriunaitis@gmail.com>
2023-06-30 12:56:37 +0300

    add header data into res items for csv loading and start working on xml loading

--- run.php
+++ run.php
@@ -1,16 +1,27 @@
 <?php
-define("DATA_DIR", "./data");
+error_reporting(E_ALL);
 
-function loadCSV(string $fname)
+const DATA_DIR = './data';
+const CSV_MAX_LINE = 1000;
+const CSV_DELIM = ',';
+
+function loadCSV(string $fname): array
 {
     if (($handle = fopen(DATA_DIR . "/" . $fname, "r")) === false) {
         return [];
     }
 
-    $res = [];
-    
-    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-        $res[] = $data;
+    if (($header = fgetcsv($handle, CSV_MAX_LINE, CSV_DELIM)) === false) {
+        return [];
+    }
+
+    while (($data = fgetcsv($handle, CSV_MAX_LINE, CSV_DELIM)) !== false) {
+        $item = [];
+        foreach ($data as $index => $val) {
+            $key = $header[$index];
+            $item[$key] = $val;
+        }
+        $res[] = $item;
     }
 
     fclose($handle);
@@ -18,9 +29,25 @@ function loadCSV(string $fname)
     return $res;
 }
 
-function run(string $fname)
+function loadXML(string $fname): array
 {
-    $data = loadCSV($fname);
+    print('loading XML');
+
+    return [];
+}
+
+function run(string $fname): int
+{
+    [$name, $ext] = explode('.', $fname);
+
+    switch ($ext) {
+    case 'csv':
+        $data = loadCSV($fname); break;
+    case 'xml':
+        $data = loadXML($fname); break;
+    default:
+        $data = [];
+    }
 
     print_r($data);
 
